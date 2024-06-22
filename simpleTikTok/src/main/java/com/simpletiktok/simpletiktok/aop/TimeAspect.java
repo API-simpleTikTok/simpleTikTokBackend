799d28a4dedb289ea83777267cf32f213e00ccfe
silvadreamer <1957915633@qq.com>
2024-06-22 18:03:01 +0800

    日志

--- simpleTikTok/src/main/java/com/simpletiktok/simpletiktok/aop/TimeAspect.java
+++ simpleTikTok/src/main/java/com/simpletiktok/simpletiktok/aop/TimeAspect.java
@@ -1,18 +1,20 @@
 package com.simpletiktok.simpletiktok.aop;
 
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class TimeAspect {
 
+    private static final Logger logger = LoggerFactory.getLogger(TimeAspect.class);
     private ThreadLocal<Long> startTime = new ThreadLocal<>();
 
     @Pointcut("@annotation(com.simpletiktok.simpletiktok.annotation.Time)")
@@ -22,7 +24,7 @@ public class TimeAspect {
     @Before("annotationPointcut()")
     public void beforePointcut(JoinPoint joinPoint) {
         startTime.set(System.currentTimeMillis());
-        System.out.println("准备执行方法: " + joinPoint.getSignature().getName());
+        logger.info("准备执行方法: {}", joinPoint.getSignature().getName());
     }
 
     @After("annotationPointcut()")
@@ -30,6 +32,7 @@ public class TimeAspect {
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime.get();
         startTime.remove();  // 清理ThreadLocal变量，避免内存泄漏
-        System.out.println("方法 " + joinPoint.getSignature().getName() + " 执行结束，耗时: " + duration + " ms");
+
+        logger.info("方法 {} 执行结束，耗时: {} ms", joinPoint.getSignature().getName(), duration);
     }
 }
